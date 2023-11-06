72385f52c1c5d42196e05ae56f39b9de2adda91b
dzelle-tan <dzellefaith@gmail.com>
2023-11-06 21:13:19 +0800

    [Refactor][Dz] - Update folder structure

--- components/landing-page/what-g-connect-does-section.tsx
+++ components/landing-page/what-g-connect-does-section.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const WhatGConnectDoesSection = () => {
-  return (
-    <section>
-      <h2>What G-Connect Does</h2>
-      <p>Insert your content here</p>
-    </section>
-  );
-};
-
-export default WhatGConnectDoesSection;
