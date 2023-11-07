a652d0207a6f632c04777442e2310da85f2859cd
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-07 22:19:03 +0800

    [Feature][Adrian] Add mobile layout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,11 +2,13 @@ import WhatGConnectDoes from "@/components/landing-page/what-g-connect-does-sect
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bg-gray-500 p-20 rounded-lg transition-all ease-in-out duration-200 hover:bg-green-500">
-        Home
-      </h1>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="bg-gray-500 p-20 rounded-lg transition-all ease-in-out duration-200 hover:bg-green-500">
+          Home
+        </h1>
+      </main>
       <WhatGConnectDoes />
-    </main>
+    </>
   );
 }
--- components/landing-page/what-g-connect-does-section.tsx
+++ components/landing-page/what-g-connect-does-section.tsx
@@ -1,54 +1,67 @@
 import Image from "next/image";
+import { Container } from "../ui/container";
 
 export default function WhatGConnectDoes() {
-    return (
-        <div className="text-neutral-600">
-            <div className="w-screen bg-indigo-100">
-                <div className="flex justify-center items-center text-center flex-col">
-                    <h2 className="text-6xl font-semibold mb-8 mt-20">
-                        What <span className="text-orange-400">G-Connect</span> Does
-                    </h2>
-                    <p className="text-3xl text-23 mb-36">
-                        Find and connect with study buddies.
-                    </p>
-                </div>
-                <div className="flex flex-wrap justify-center items-center space-x-20">
-                    <div className="">
-                        <Image
-                            src="gconnect-home-preview.svg"
-                            alt="Home Preview"
-                            width={951.51}
-                            height={621.71}
-                        />
-                    </div>
-                    <div className="w-[28%]">
-                        <p className="text-4xl font-bold mb-9">
-                            Find a study buddy who is compatible with you
-                        </p>
-                        <p className="text-2xl">
-                            G-Connect matches you with other students based on your courses, interests, and availability. This means that you're more likely to find a study buddy who is a good fit for you.
-                        </p>
-                    </div>
-                </div>
+  return (
+    <section className="text-neutral-600 w-full">
+      <div className="bg-indigo-100">
+        <Container>
+          <div className="flex justify-center items-center text-center flex-col mb-10">
+            <h2 className="text-2xl md:text-4xl font-semibold pt-10 md:pt-20">
+              What <span className="text-orange-400">G-Connect</span> Does?
+            </h2>
+            <p className="text-md lg:text-lg text-zinc-400">
+              Find and connect with study buddies.
+            </p>
+          </div>
+          <div className="flex justify-center items-center flex-col-reverse lg:flex-row ">
+            <Image
+              src="gconnect-home-preview.svg"
+              alt="Home Preview"
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-[100%] lg:w-[50%] h-auto rounded-t-lg"
+            />
+            <div className="mb-6 basis-[40%] md:p-0 lg:p-10 flex flex-col justify-center items-start">
+              <p className="text-lg w-full lg:w-72 text-left font-bold mb-4">
+                Find a study buddy who is compatible with you
+              </p>
+              <p className="text-md text-left">
+                G-Connect matches you with other students based on your courses,
+                interests, and availability. This means that you&apos;re more
+                likely to find a study buddy who is a good fit for you.
+              </p>
             </div>
-            <div className="flex flex-wrap justify-center items-center space-x-20">
-                <div className="">
-                    <Image
-                        src="gconnect-messages-preview.svg"
-                        alt="Messages Preview"
-                            width={951.51}
-                            height={621.71}
-                    />
-                </div>
-                <div className="w-96 w-[28%]">
-                    <p className="text-4xl font-bold mb-9">
-                        Get help with your coursework
-                    </p>
-                    <p className="text-2xl">
-                        If you're struggling with a particular course, your study buddy can help you to understand the material and practice answering questions.
-                    </p>
-                </div>
+          </div>
+        </Container>
+      </div>
+
+      <div className="mb-10">
+        <Container>
+          <div className="flex justify-center items-center flex-col lg:flex-row ">
+            <Image
+              src="gconnect-messages-preview.svg"
+              alt="Messages Preview"
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-[100%] lg:w-[50%] h-auto rounded-b-lg shadow-md"
+            />
+
+            <div className="mt-6 basis-[40%] md:p-0 lg:p-10 flex flex-col justify-center items-start">
+              <p className="text-lg w-full lg:w-72 text-left font-bold mb-4">
+                Get help with your coursework
+              </p>
+              <p className="text-md text-left">
+                If you&apos;re struggling with a particular course, your study
+                buddy can help you to understand the material and practice
+                answering questions.
+              </p>
             </div>
-        </div>
-    );
+          </div>
+        </Container>
+      </div>
+    </section>
+  );
 }
