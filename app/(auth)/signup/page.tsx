8e754b011747d142e4a6c9958a9f632622634711
Dzelle Faith Tan <84973759+dzelle-tan@users.noreply.github.com>
2023-11-17 21:45:51 +0800

    [Feature][Dz] - Add Sign-Up Page (#13)
    * [Feature][Dz] - Sign up page structure
    
    * [Style][Dz] - Style sign up page
    
    * [Fix][Adrian] Fix nested layout showing root layout navbar and footer
    
    * [Misc][Dz] - Comment out username
    
    * [Refactor][Adrian]
    
    * [Refactor][Adrian]
    
    ---------
    
    Co-authored-by: drianlarde <aadabelarde2020@plm.edu.ph>

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Container } from '@/components/ui/container';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Mail } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: 'Invalid email address',
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.',
+  }),
+  // Might need to add username later
+  // username: z.string().min(2, {
+  //     message: "Username must be at least 2 characters.",
+  // }),
+});
+
+export default function ProfileForm() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = () => {
+    // Handle form submission
+    console.log();
+  };
+
+  return (
+    <Container className='flex flex-col h-screen w-full items-center justify-center'>
+      <Image
+        src='/gconnect-logo.svg'
+        alt='GConnect Logo'
+        width='0'
+        height='0'
+        sizes='100vw'
+        className='h-auto w-[4rem] mb-20 object-cover transition-all duration-100 ease-in-out hover:animate-pulse'
+      />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-[18.75rem]'
+        >
+          <div className='text-center'>
+            <h1 className='text-2xl lg:text-4xl font-semibold mb-2'>Sign Up</h1>
+            <p className='text-zinc-400 mb-10'>Use your University Email</p>
+          </div>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='yourname@university.edu' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type='password' placeholder='••••••••' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' variant='purple' className='w-full'>
+            <Mail className='mr-2 h-4 w-4' /> Sign up using University Email
+          </Button>
+          <p className='text-center'>
+            or{' '}
+            <Link href='/login' className='font-semibold'>
+              Login
+            </Link>{' '}
+            instead.
+          </p>
+        </form>
+      </Form>
+    </Container>
+  );
+}
