3cc891d79377481fbde7666c20c8ab7aa2012850
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-17 17:38:19 +0800

    [Fix][Adrian] Fix nested layout showing root layout navbar and footer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image';
 
+import '../globals.css';
+
 export default function AuthLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-return (
-    <main className="min-h-screen flex flex-col items-center text-neutral-600 ">
-        <Image
-            src='gconnect-logo.svg'
-            alt='GConnect Logo'
-            width={120}
-            height={120}
-            className='my-10 w-24 lg:w-32'
-        />
-        {children}
+  return (
+    <main className='min-h-screen flex flex-col items-center text-neutral-600 '>
+      <Image
+        src='gconnect-logo.svg'
+        alt='GConnect Logo'
+        width={120}
+        height={120}
+        className='my-10 w-24 lg:w-32'
+      />
+      {children}
     </main>
-);
+  );
 }
