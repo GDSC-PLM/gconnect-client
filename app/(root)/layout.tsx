0135f50fc75c2dd8800faf07317825adb2e87093
Joshua Caleb Bolito <88492918+JCBolito@users.noreply.github.com>
2023-11-17 21:20:11 +0800

    [Feature][Joshua] - Added Get Started section (#12)
    * [Feature][Joshua] - Added Get Started section
    
    * [Refactor][Adrian]
    
    ---------
    
    Co-authored-by: drianlarde <aadabelarde2020@plm.edu.ph>

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/layout/navbar';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import '../globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
