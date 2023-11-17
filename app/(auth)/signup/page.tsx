c3860da15a4c375bb8d48272b4e39951e85a1616
dzelle-tan <dzellefaith@gmail.com>
2023-11-17 17:46:52 +0800

    [Misc][Dz] - Comment out username

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -18,15 +18,16 @@ import {
 import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
     email: z.string().email({
         message: "Invalid email address",
     }),
     password: z.string().min(8, {
         message: "Password must be at least 8 characters.",
     }),
+    // might need to add username later
+    // username: z.string().min(2, {
+    //     message: "Username must be at least 2 characters.",
+    // }),
 })
 
 export default function ProfileForm() {
